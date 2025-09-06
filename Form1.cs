9f443646f7b751824bf8a68d9aa9c010180b3dd6
enzorom06 <romeroenzo288@gmail.com>
2025-09-06 18:35:34 -0300

    Cambios,Menu Lateral.

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,22 @@ namespace Cumbre_Libros
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BReportes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BBackup_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
